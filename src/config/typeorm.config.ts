3ce2af37b7d99783a019673b27a558a2a3a09e9b
Edwin Tsatsu <paynegreen@yahoo.com>
2019-09-09 01:04:17 +0000

    fix start:dev issue

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
+import { join } from 'path';
 
 const dbConfig = config.get('db');
 
@@ -10,6 +11,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
-  entities: [__dirname + '/../**/*.entity.ts'],
+  entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+  logging: true,
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
